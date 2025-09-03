6361e8a020166297f65f17a0cc5d8855f6395823
DiegoiSaavedra <elsaavedrasabe@gmail.com>
2025-09-03 20:30:30 -0300

    Fix duplicated laser transforms

--- src/robot_base/launch/static_tf.launch.py
+++ src/robot_base/launch/static_tf.launch.py
@@ -9,7 +9,7 @@ def generate_launch_description():
             package='tf2_ros',
             executable='static_transform_publisher',
             name='base_to_laser_tf',
-            arguments=['0.0', '0.0', '0.18', '0.0', '0.0', '0.0', 'base_link', 'laser']
+            arguments=['0.0', '0.0', '0.18', '0.0', '0.0', '0.0', 'base_link', 'base_laser']
         )
     ])
 
